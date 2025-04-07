dca09e24f4208ab2c81eb95147fedbc4fd15da8c
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-07 10:56:44 +0400

    chore: Add TaskHistory entity to the system for historically accurate report generation.

---
+++ Domain/Entities/HistoricalRecords/TaskHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities.History;
+
+using Domain.Entities.Base;
+using Domain.Enumerations;
+
+public class TaskHistory : HistoricalEntity
+{
+    public Guid TaskId { get; set; }
+    public TaskStatus Status { get; set; }
+    public TaskPriority Priority { get; set; }
+    public TaskType TaskType { get; set; }
+    public DateTime StatusChangedAt { get; set; }
+}
