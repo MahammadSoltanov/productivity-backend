f0bcf2a81ccdb9c7462dbf5e0452cc14d7f513a2
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-07 10:54:13 +0400

    chore: Add auditable base class entity to the system.

---
+++ Domain/Entities/Base/AutditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities.Base;
+public class AuditableEntity : Entity
+{
+    public string CreatedBy { get; set; }
+    public string ModifiedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ModifiedAt { get; set; }
+}
