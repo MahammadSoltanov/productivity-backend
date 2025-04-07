d6c42be315e1dd2b9b089b07268814a1e63efbbe
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-07 11:02:43 +0400

    chore: Add Attachment and User entities to the system.

---
+++ Domain/Entities/Attachment.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+using Domain.Entities.Base;
+
+public class Attachment : Entity
+{
+    public string FileName { get; set; }
+    public string FileUrl { get; set; }
+    public DateTime UploadedAt { get; set; }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Base;
+using Domain.Enumerations;
+
+namespace Domain.Entities;
+
+public sealed class User : Entity
+{
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string Email { get; set; }
+    public string? PasswordHash { get; set; }
+    public AuthenticationProvider AuthenticationProvider { get; set; }
+}
