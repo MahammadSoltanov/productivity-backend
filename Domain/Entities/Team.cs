53f73ce5c8b5dcbb2bd4caad682a7b6d526c51c4
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-11 18:58:52 +0400

    chore: Add Team and Workspace entities to the system.

---
+++ Domain/Entities/Team.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Base;
+using Domain.Entities.HistoricalRecords;
+
+namespace Domain.Entities;
+
+public sealed class Team : AuditableEntity
+{
+    public Team(string title)
+    {
+        Title = title;
+    }
+
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public ICollection<UserTeamMembership> Members { get; set; } = new List<UserTeamMembership>();
+    public ICollection<TeamWorkspaceMembership> WorkspaceMemberships { get; set; } = new List<TeamWorkspaceMembership>();
+}
---
+++ Domain/Entities/Workspace.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities;
+
+public sealed class Workspace : AuditableEntity
+{
+    public Workspace(string name, User creator)
+    {
+        Name = name;
+        Creator = creator;
+    }
+
+    public string Name { get; set; }
+    public string? Desciption { get; set; }
+    public Guid CreatorId { get; set; }
+    public User Creator { get; set; }
+    public Guid? DefaultAssigneeId { get; set; }
+    public User? DefaultAssignee { get; set; }
+}
