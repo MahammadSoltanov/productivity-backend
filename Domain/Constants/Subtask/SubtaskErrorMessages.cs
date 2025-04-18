bb03c386c9b972ac78d7fb65861698a1f7cd0112
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-18 17:50:18 +0400

    style: Fix property name.

--- Domain/Constants/Subtask/SubtaskErrorMessages.cs
+++ Domain/Constants/Subtask/SubtaskErrorMessages.cs
@@ -2,5 +2,5 @@
 
 internal static class SubtaskErrorMessages
 {
-    public static string InvalidSutaskDependency = "Subtasks can only have dependencies of type Subtask.";
+    public static string InvalidSubtaskDependency = "Subtasks can only have dependencies of type Subtask.";
 }
