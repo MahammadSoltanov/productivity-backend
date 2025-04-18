dda0bca005fd93d749bbe6dcfd0efc6e01093866
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-18 09:50:41 +0400

    chore: Add Company entity to the domain layer.

---
+++ Domain/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities.Base;
+
+namespace Domain.Entities;
+
+public class Company : AuditableEntity
+{
+    public Company(string name, Guid ownerId)
+    {
+        Name = name;
+        OwnerId = ownerId;
+    }
+
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public ICollection<Workspace> Workspaces { get; set; } = new List<Workspace>();
+    public ICollection<Team> Teams { get; set; } = new List<Team>();
+    public Guid OwnerId { get; set; }
+    public User? Owner { get; set; }
+}
