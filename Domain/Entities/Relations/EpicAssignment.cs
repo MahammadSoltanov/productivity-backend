b1d44c5ed64d27bd0ffac81da2de99725feec63c
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-25 20:20:01 +0400

    chore: Remove TaskAssignment class from the system and add assignment relationship entity for each task type.

---
+++ Domain/Entities/Relations/EpicAssignment.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities.Relations;
+
+public class EpicAssignment
+{
+    public EpicAssignment(Guid epicId, Guid assigneeId)
+    {
+        EpicId = epicId;
+        AssigneeId = assigneeId;
+    }
+
+    public Guid EpicId { get; private set; }
+    public Epic Epic { get; private set; }
+    public Guid AssigneeId { get; private set; }
+    public User Assignee { get; private set; }
+}
