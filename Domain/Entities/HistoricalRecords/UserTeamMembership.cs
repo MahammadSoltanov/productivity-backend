b5a8e21a5716a57750e39dfa175883c5e21b639a
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-04-28 17:50:39 +0400

    chore: Add membership specific role properties to the membership entities for teams and workspaces.

--- Domain/Entities/HistoricalRecords/UserTeamMembership.cs
+++ Domain/Entities/HistoricalRecords/UserTeamMembership.cs
@@ -1,17 +1,20 @@
 ﻿using Domain.Entities.Base;
+using Domain.Enumerations;
 
 namespace Domain.Entities.HistoricalRecords;
 
 public sealed class UserTeamMembership : HistoricalEntity
 {
-    public UserTeamMembership(Guid teamId, Guid userId)
+    public UserTeamMembership(Guid teamId, Guid userId, TeamRole role)
     {
         TeamId = teamId;
         UserId = userId;
+        Role = role;
     }
 
     public Guid UserId { get; private set; }
-    public User User { get; private set; }
+    public User? User { get; private set; }
     public Guid TeamId { get; private set; }
-    public Team Team { get; private set; }
+    public Team? Team { get; private set; }
+    public TeamRole Role { get; private set; }
 }
