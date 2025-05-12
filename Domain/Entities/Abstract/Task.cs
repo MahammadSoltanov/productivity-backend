eb5938ab8f1bfb12bbf0bea4a187c63c1c666c31
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-05-12 12:54:05 +0400

    chore: Improve encapsulation of the Task entity properties.

--- Domain/Entities/Abstract/Task.cs
+++ Domain/Entities/Abstract/Task.cs
@@ -15,18 +15,23 @@ public abstract class Task : AuditableEntity
 
     public string Title { get; set; }
     public string? Description { get; set; }
-    public TaskStatus Status { get; private set; } = TaskStatus.Pending;
+
+    public TaskStatus Status { get; protected set; } = TaskStatus.Pending;
     public TaskPriority Priority { get; set; } = TaskPriority.Medium;
-    public Guid WorkspaceId { get; private set; }
-    public Workspace? Workspace { get; private set; }
-    public Guid CreatorId { get; set; }
-    public User? Creator { get; set; }
-    public Guid AssigneeId { get; set; }
-    public User? Assignee { get; set; }
-    public DateTime EstimatedStartDate { get; set; }
-    public DateTime EstimatedCompletionDate { get; set; }
-    public DateTime ActualStartDate { get; set; }
-    public DateTime ActualCompletionDate { get; set; }
+
+    public User? Creator { get; protected set; }
+
+    public Guid WorkspaceId { get; protected set; }
+    public Workspace? Workspace { get; protected set; }
+
+    public Guid AssigneeId { get; protected set; }
+    public User? Assignee { get; protected set; }
+
+    public DateTime EstimatedStartDate { get; protected set; }
+    public DateTime EstimatedCompletionDate { get; protected set; }
+    public DateTime ActualStartDate { get; protected set; }
+    public DateTime ActualCompletionDate { get; protected set; }
+
     public ICollection<string> Tags { get; set; } = new List<string>();
     public ICollection<TaskAttachment> TaskAttachments { get; set; } = new List<TaskAttachment>();
     public ICollection<TaskDependency> TaskDependencies { get; set; } = new List<TaskDependency>();
