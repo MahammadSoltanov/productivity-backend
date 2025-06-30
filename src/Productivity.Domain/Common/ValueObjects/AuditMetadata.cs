af74e0add9fe1bd8d0c15da90f173371e06c83db
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-06-30 17:57:25 +0400

    chore: Replace AuditableEntity with a AuditableMetadata value object.

---
+++ src/Productivity.Domain/Common/ValueObjects/AuditMetadata.cs
@@ -0,0 +1,10 @@
+﻿namespace Productivity.Domain.Common.ValueObjects;
+
+public record AuditMetadata(
+    UserId CreatorId,
+    DateTime CreatedAt,
+    UserId? ModifierId = null,
+    DateTime? ModifiedAt = null)
+{
+    public AuditMetadata WithModification(UserId modifier, DateTime when) => this with { ModifierId = modifier, ModifiedAt = when };
+}
