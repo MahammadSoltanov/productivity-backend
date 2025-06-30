4b7ea807f251ff892f4bbd0220d9a1e3e1b08f72
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-06-30 17:49:05 +0400

    chore: Modified TaskHistory entity to be generic and accept id types.

---
+++ src/Productivity.Domain/Common/Abstract/TaskStateHistoryEntry.cs
@@ -0,0 +1,21 @@
+﻿using Productivity.Domain.Common.Models;
+using Productivity.Domain.Common.ValueObjects;
+using Productivity.Domain.Enumerations;
+
+namespace Productivity.Domain.Common.Abstract;
+
+public abstract class TaskStateHistoryEntry<TId, TTaskId> : Entity<TId>
+{
+    public TTaskId TaskId { get; }
+    public DateRange ValidityPeriod { get; }
+    public TaskStatus Status { get; }
+    public TaskPriority Priority { get; }
+
+    public TaskStateHistoryEntry(TId id, TTaskId taskId, TaskStatus status, TaskPriority priority, DateRange validityPeriod) : base(id)
+    {
+        TaskId = taskId;
+        Status = status;
+        Priority = priority;
+        ValidityPeriod = validityPeriod;
+    }
+}
