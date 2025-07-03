970a39d4362ae57e39f5176c7286d1ba5f2bf07f
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-07-03 18:37:45 +0400

    chore: Removed UserCompanyMembership entity from the domain as it doesn't pose any meaning and won't in the future based on the documentation. The persistence concer of many to many relationship between company and user is left on the Infrastructure layer.

--- src/Productivity.Domain/CompanyAggregate/Company.cs
+++ src/Productivity.Domain/CompanyAggregate/Company.cs
@@ -1,12 +1,11 @@
 ﻿using Productivity.Domain.Common.Models;
 using Productivity.Domain.Common.ValueObjects;
-using Productivity.Domain.CompanyAggregate.Entities;
 
 namespace Productivity.Domain.CompanyAggregate;
 
 public sealed class Company : AggregateRoot<CompanyId>
 {
-    private readonly List<UserCompanyMembership> _users = new();
+    private readonly List<UserId> _users = new();
     private readonly List<WorkspaceId> _workspaces = new();
     private readonly List<TeamId> _teams = new();
 
@@ -14,7 +13,7 @@ public sealed class Company : AggregateRoot<CompanyId>
     public string? Description { get; set; }
     public ICollection<WorkspaceId> Workspaces => _workspaces.AsReadOnly();
     public ICollection<TeamId> Teams => _teams.AsReadOnly();
-    public IReadOnlyCollection<UserCompanyMembership> Users => _users.AsReadOnly();
+    public IReadOnlyCollection<UserId> Users => _users.AsReadOnly();
     public UserId OwnerId { get; }
     public AuditMetadata AuditMetadata { get; }
 
