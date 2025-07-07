3f3057dddeb7a6d8cfcdb3b3b3727ea8185edfe8
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-07-07 16:45:55 +0400

    chore: Add user company membership entity to the company aggregate.

---
+++ src/Productivity.Domain/CompanyAggregate/Entities/UserCompanyMembership.cs
@@ -0,0 +1,20 @@
+﻿using Productivity.Domain.Common.Models;
+using Productivity.Domain.CompanyAggregate.Enumerations;
+
+namespace Productivity.Domain.CompanyAggregate.Entities;
+public class UserCompanyMembership : Entity<UserCompanyMembershipId>
+{
+    public UserId UserId { get; }
+    public CompanyRole Role { get; private set; }
+
+    public UserCompanyMembership(UserCompanyMembershipId id, UserId userId, CompanyRole role) : base(id)
+    {
+        UserId = userId;
+        Role = role;
+    }
+
+    public static UserCompanyMembership Create(UserId userId, CompanyRole role)
+    {
+        return new(UserCompanyMembershipId.CreateUnique(), userId, role);
+    }
+}
---
+++ src/Productivity.Domain/CompanyAggregate/ValueObjects/UserCompanyMembershipId.cs
@@ -0,0 +1,22 @@
+﻿using Productivity.Domain.Common.Models;
+
+namespace Productivity.Domain.CompanyAggregate.ValueObjects;
+public sealed class UserCompanyMembershipId : ValueObject
+{
+    public Guid Value { get; }
+
+    private UserCompanyMembershipId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static UserCompanyMembershipId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
