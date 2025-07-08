a4223e9d2160345975dffa497dd3042cbfb5ac39
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-07-08 11:26:47 +0400

    chore: Add lifecycle methods, properties and improved encapsulation of the UserCompanyMembership.

--- src/Productivity.Domain/CompanyAggregate/Entities/UserCompanyMembership.cs
+++ src/Productivity.Domain/CompanyAggregate/Entities/UserCompanyMembership.cs
@@ -1,4 +1,7 @@
-﻿using Productivity.Domain.Common.Models;
+﻿using Productivity.Domain.Common.Enumerations;
+using Productivity.Domain.Common.Exceptions;
+using Productivity.Domain.Common.Models;
+using Productivity.Domain.Common.ValueObjects;
 using Productivity.Domain.CompanyAggregate.Enumerations;
 
 namespace Productivity.Domain.CompanyAggregate.Entities;
@@ -6,15 +9,30 @@ public class UserCompanyMembership : Entity<UserCompanyMembershipId>
 {
     public UserId UserId { get; }
     public CompanyRole Role { get; private set; }
+    public MembershipStatus Status { get; private set; }
+    public DateRange ValidityPeriod { get; private set; }
 
-    public UserCompanyMembership(UserCompanyMembershipId id, UserId userId, CompanyRole role) : base(id)
+    private UserCompanyMembership(UserCompanyMembershipId id, UserId userId, CompanyRole role) : base(id)
     {
         UserId = userId;
         Role = role;
+        ValidityPeriod = new DateRange(DateTime.UtcNow);
     }
 
     public static UserCompanyMembership Create(UserId userId, CompanyRole role)
     {
         return new(UserCompanyMembershipId.CreateUnique(), userId, role);
+    }
+
+    public void End()
+    {
+        if (Status != MembershipStatus.Active)
+        {
+            throw new DomainException("Only active memberships can be ended.");
+        }
+
+        Status = MembershipStatus.Removed;
+
+        ValidityPeriod = new DateRange(ValidityPeriod.From, DateTime.UtcNow);
     }
 }
