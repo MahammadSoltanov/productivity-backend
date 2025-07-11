f738d0d92107e26bf56e1cc6b13778d7598515cb
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-07-11 10:40:18 +0400

    chore: Define and added initial model of global workspace settings and user specific workspace settings to the system.

---
+++ src/Productivity.Domain/WorkspaceAggregate/ValueObjects/GlobalWorkspaceSettings.cs
@@ -0,0 +1,34 @@
+﻿using Productivity.Domain.Common.Enumerations;
+using Productivity.Domain.Common.Models;
+
+namespace Productivity.Domain.WorkspaceAggregate.ValueObjects;
+public sealed class GlobalWorkspaceSettings : ValueObject
+{
+    public TaskViewMode DefaultTaskView { get; }
+    public TimeSpan WorkDayStart { get; }
+    public TimeSpan WorkDayEnd { get; }
+    public int DefaultPageSize { get; }
+    public IReadOnlySet<NotificationChannel> DefaultNotificationChannels { get; }
+
+    public GlobalWorkspaceSettings(TaskViewMode defaultTaskView,
+                                   TimeSpan workDayStart,
+                                   TimeSpan workDayEnd,
+                                   int defaultPageSize,
+                                   IEnumerable<NotificationChannel> defaultNotificationChannels)
+    {
+        DefaultTaskView = defaultTaskView;
+        WorkDayStart = workDayStart;
+        WorkDayEnd = workDayEnd;
+        DefaultPageSize = defaultPageSize;
+        DefaultNotificationChannels = new HashSet<NotificationChannel>(defaultNotificationChannels);
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return DefaultTaskView;
+        yield return WorkDayStart;
+        yield return WorkDayEnd;
+        yield return DefaultPageSize;
+        foreach (var c in DefaultNotificationChannels) yield return c;
+    }
+}
---
+++ src/Productivity.Domain/WorkspaceAggregate/ValueObjects/UserWorkspaceSettings.cs
@@ -0,0 +1,38 @@
+﻿using Productivity.Domain.Common.Enumerations;
+using Productivity.Domain.Common.Models;
+
+namespace Productivity.Domain.WorkspaceAggregate.ValueObjects;
+public sealed class UserWorkspaceSettings : ValueObject
+{
+    public TaskViewMode? PreferredTaskView { get; }
+    public int? PageSize { get; }
+    public IReadOnlySet<NotificationChannel> NotificationChannels { get; }
+    public IReadOnlySet<NotificationType> MutedNotificationTypes { get; }
+    public IReadOnlySet<string> CollapsedSections { get; }
+
+    public UserWorkspaceSettings(TaskViewMode? preferredTaskView,
+                                 int? pageSize,
+                                 IEnumerable<NotificationChannel> notificationChannels,
+                                 IEnumerable<NotificationType>? mutedNotificationTypes,
+                                 IEnumerable<string>? collapsedSections)
+    {
+        PreferredTaskView = preferredTaskView;
+        PageSize = pageSize;
+        NotificationChannels = new HashSet<NotificationChannel>(notificationChannels);
+        MutedNotificationTypes = mutedNotificationTypes is null
+                                  ? new HashSet<NotificationType>()
+                                  : new HashSet<NotificationType>(mutedNotificationTypes);
+        CollapsedSections = collapsedSections is null
+                                  ? new HashSet<string>()
+                                  : new HashSet<string>(collapsedSections);
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        if (PreferredTaskView.HasValue) yield return PreferredTaskView.Value;
+        if (PageSize is not null) yield return PageSize.Value;
+        foreach (var c in NotificationChannels) yield return c;
+        foreach (var t in MutedNotificationTypes) yield return t;
+        foreach (var s in CollapsedSections) yield return s;
+    }
+}
