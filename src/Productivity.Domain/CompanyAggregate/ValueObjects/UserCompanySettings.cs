21d37a165cbf165b7c013590835a37cc6903727d
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-07-11 10:12:23 +0400

    chore: Add UserCompanySettings for user specific settings on each company.

---
+++ src/Productivity.Domain/CompanyAggregate/ValueObjects/UserCompanySettings.cs
@@ -0,0 +1,33 @@
+﻿using Productivity.Domain.Common.Enumerations;
+using Productivity.Domain.Common.Models;
+
+namespace Productivity.Domain.CompanyAggregate.ValueObjects;
+public sealed class UserCompanySettings : ValueObject
+{
+    public IReadOnlySet<NotificationChannel> NotificationChannels { get; }
+    public IReadOnlySet<NotificationType> MutedNotificationTypes { get; }
+    public TaskViewMode DefaultTaskView { get; }
+    public bool ShowCompanyAnnouncements { get; }
+
+    public UserCompanySettings(
+        IEnumerable<NotificationChannel> notificationChannels,
+        IEnumerable<NotificationType>? mutedNotificationTypes,
+        TaskViewMode defaultTaskView,
+        bool showCompanyAnnouncements)
+    {
+        NotificationChannels = new HashSet<NotificationChannel>(notificationChannels);
+        MutedNotificationTypes = mutedNotificationTypes is null
+                                   ? new HashSet<NotificationType>()
+                                   : new HashSet<NotificationType>(mutedNotificationTypes);
+        DefaultTaskView = defaultTaskView;
+        ShowCompanyAnnouncements = showCompanyAnnouncements;
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        foreach (var c in NotificationChannels) yield return c;
+        foreach (var t in MutedNotificationTypes) yield return t;
+        yield return DefaultTaskView;
+        yield return ShowCompanyAnnouncements;
+    }
+}
