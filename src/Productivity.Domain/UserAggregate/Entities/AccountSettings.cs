00a59ba4baedd3998e15647d4547b79f81c18a9d
Mahammad Soltanov <maqa.sltnv@gmail.com>
2025-07-11 10:13:18 +0400

    chore: Included additional options for user account settings.

--- src/Productivity.Domain/UserAggregate/Entities/AccountSettings.cs
+++ src/Productivity.Domain/UserAggregate/Entities/AccountSettings.cs
@@ -4,17 +4,26 @@ using Productivity.Domain.Common.Models;
 namespace Productivity.Domain.UserAggregate.Entities;
 public sealed class AccountSettings : ValueObject
 {
+    public ThemePreference Theme { get; }
+    public int ItemsPerPage { get; }
+    public TimeZoneInfo? DefaultTimeZone { get; }
     public IReadOnlySet<NotificationChannel> EnabledChannels { get; }
     public IReadOnlySet<NotificationType> MutedNotificationTypes { get; }
 
     public AccountSettings(
       IEnumerable<NotificationChannel> channels,
-      IEnumerable<NotificationType>? mutedTypes = null)
+      IEnumerable<NotificationType>? muted,
+      ThemePreference theme,
+      int itemsPerPage,
+      TimeZoneInfo? defaultTimeZone = null)
     {
         EnabledChannels = new HashSet<NotificationChannel>(channels);
-        MutedNotificationTypes = mutedTypes is null
+        MutedNotificationTypes = muted is null
           ? new HashSet<NotificationType>()
-          : new HashSet<NotificationType>(mutedTypes);
+          : new HashSet<NotificationType>(muted);
+        Theme = theme;
+        ItemsPerPage = itemsPerPage;
+        DefaultTimeZone = defaultTimeZone;
     }
 
     public override IEnumerable<object> GetEqualityComponents()
